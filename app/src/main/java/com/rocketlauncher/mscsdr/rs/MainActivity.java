e4d8c356f277188f528b19a00990405957402b14
Mathias Steger <mathias.steger@uni-oldenburg.com>
2018-12-16 14:28:26 +0100

    Initial commit

---
+++ app/src/main/java/com/rocketlauncher/mscsdr/rs/MainActivity.java
@@ -0,0 +1,25 @@
+package com.rocketlauncher.mscsdr.rs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSingle(View view) {
+        Intent intent = new Intent(MainActivity.this, SingleActivity.class);
+        startActivity(intent);
+    }
+
+    public void onMulti(View view) {
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
