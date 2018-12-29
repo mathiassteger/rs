02edc4f9d61f4c99d21515842d5fdfc179e0e9ee
Christian S <christians@Christians-MacBook-Pro.local>
2018-12-29 19:59:04 +0100

    Socket wird nach Bluetooth error erneut initialisiert

--- PiProgramm.py
+++ PiProgramm.py
@@ -6,15 +6,24 @@ port = 1
 server_socket.bind(("",port))
 server_socket.listen(1)
 
-client_socket,address = server_socket.accept()
-print "Accepted connection from ",address
-while 1:
+def init():
+ client_socket,address = server_socket.accept()
+ print "Accepted connection from ",address
+ while 1:
 
- data = client_socket.recv(1024)
- print "Received: %s" % data
- if (data == "q"):
-  print ("Quit")
-  break
+  data = client_socket.recv(1024)
+  print "Received: %s" % data
+  if data == "q":
+   print ("Quit")
+   break
 
-client_socket.close()
-server_socket.close()
+ client_socket.close()
+ server_socket.close()
+
+if __name__ == "__main__":
+
+ while 1:
+  try:
+   init()
+  except bluetooth.btcommon.BluetoothError:
+   init()
